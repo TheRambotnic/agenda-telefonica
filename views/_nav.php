b8139c897270ef33504b0d2fc739d967c4338ee7
Lucas Rafael <lucasrafaellonguini09@gmail.com>
2021-01-19 18:27:15 -0300

    adicionado ícone do sistema

--- views/_nav.php
+++ views/_nav.php
@@ -5,9 +5,9 @@
 		<meta name="author" content="Lucas Rafael de Oliveira Longuini" />
 		
 		<link rel="stylesheet" type="text/css" href="views/assets/css/layout.css" />
-		<!-- <link rel="stylesheet" type="text/css" href="views/assets/font-awesome-5.10/css/all.css"/> -->
 		<link rel="stylesheet" type="text/css" href="views/assets/fontawesome-5.15-pro/css/all.css"/>
 		<link rel="stylesheet" type="text/css" href="views/assets/js/jquery/plugins/tables.css" />
+		<link rel="shortcut icon" type="image/png" href="views/assets/img/icon.png" />
 
 		<script type="text/javascript" src="views/assets/js/jquery/jquery-3.4.1.js"></script>
 		<script type="text/javascript" src="views/assets/js/jquery/plugins/datatables-1.10.js"></script>
